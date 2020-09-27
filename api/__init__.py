0890c0c5cc0e75ded15bc5d5b8f0e10dfd551bed
Moody Rahman <mrahman01@stuy.edu>
2020-09-27 08:41:37 -0400

    loan calculate and debug

--- api/__init__.py
+++ api/__init__.py
@@ -15,16 +15,36 @@ def method():
 	return {"status":200}
 
 
-@app.route("/userinfo")
+@app.route("/userinfo", methods=["GET"])
 def getuinfo():
 	out = {}
 	loanraw = requests.get(
 		'http://api.reimaginebanking.com/loans/5f70818ff1bac107157e1858?key=69f11d6f3573cbce72b64f85821c1523')
+	
 	loanjson = json.loads(loanraw.text)
 	out["loan_remaining"] = loanjson["amount"]
 	out["monthly_payment"] = loanjson["monthly_payment"]
 
 	return loanjson
+
+@app.route("/loancalculate", methods=["POST"])
+def calculate():
+	print(request.get_json())
+
+	return "it worked!"
+
+
+@app.route("/debug")
+def debug():
+	r = requests.post(
+		"http://localhost:5000/loancalculate",
+		data=json.dumps({
+			"hello":"world"
+		}),
+		headers={'content-type': 'application/json'}
+	)
+	print(r.text)
+	return "a"
 
 if __name__ == "__main__":
 	app.run(debug=True)
