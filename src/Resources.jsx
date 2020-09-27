50d3626d8a513c91bfe6653051b6c00914ff1d7f
Shanice Smith <shanicesmith.work@hotmail.com>
2020-09-26 23:32:04 -0400

    added resources cards and links

--- src/Resources.jsx
+++ src/Resources.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Resources.css";
-import { Container, Row, Col, Card, CardBody, CardImg } from "shards-react";
+import { Container, Row, Col, Card, CardBody, CardTitle, CardSubtitle, CardImg } from "shards-react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "shards-ui/dist/css/shards.min.css";
@@ -12,19 +12,21 @@ export default function Resources() {
         <Row>
           <Col>
             <Card>
-              <CardBody>
-                <p>This is the body of the card.</p>
-              </CardBody>
-              <CardImg bottom src="https://place-hold.it/300x200" />
+                <CardImg top src="https://img2.storyblok.com/760x0/f/45004/760x370/8a72d1ebc1/refinancing-blog-post.jpg" />
+                    <CardTitle><a href="https://www.snowballwealth.com/blog/what-is-student-loan-refinancing-and-when-is-a-good-time-to-" target="_blank"></a></CardTitle>
+                        <CardBody>
+                            <p>Refinance private loans as soon as you have good credit and a stable income to get a lower rate and save the most money.</p>
+                        </CardBody>
             </Card>
           </Col>
   
           <Col>
             <Card>
-              <CardBody>
-                <p>This is the body of the card.</p>
-              </CardBody>
-              <CardImg bottom src="https://place-hold.it/300x200" />
+            <CardImg top src="https://img2.storyblok.com/760x0/f/45004/760x370/8a72d1ebc1/refinancing-blog-post.jpg" />
+                    <CardTitle><a href="https://www.snowballwealth.com/blog/list-of-federal-student-loan-servicers-and-their-phone-numbers" target="_blank">List of Federal Student Loan Servicers and their phone numbers</a></CardTitle>
+                        <CardBody>
+                            <p>For when you need to contact them, here is a list of federal student loan servicers and their phone numbers.</p>
+                        </CardBody>
             </Card>
           </Col>
         </Row>
