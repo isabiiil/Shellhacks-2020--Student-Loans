0675012ba6562d9f8e7de42eb99bfcfa00187035
Shanice Smith <shanicesmith.work@hotmail.com>
2020-09-26 23:15:04 -0400

    added placeholder cards for resources

--- src/Resources.jsx
+++ src/Resources.jsx
@@ -1,4 +1,33 @@
 import React from "react";
 import "./Resources.css";
+import { Container, Row, Col, Card, CardBody, CardImg } from "shards-react";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "shards-ui/dist/css/shards.min.css";
+
+
+export default function Resources() {
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <Card>
+              <CardBody>
+                <p>This is the body of the card.</p>
+              </CardBody>
+              <CardImg bottom src="https://place-hold.it/300x200" />
+            </Card>
+          </Col>
+  
+          <Col>
+            <Card>
+              <CardBody>
+                <p>This is the body of the card.</p>
+              </CardBody>
+              <CardImg bottom src="https://place-hold.it/300x200" />
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
